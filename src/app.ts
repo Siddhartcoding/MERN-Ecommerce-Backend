d1a4542dcaa98f707b0853ea217543143bd59909
Siddharth jangir <137277893+Siddhartcoding@users.noreply.github.com>
2024-04-09 22:22:05 +0530

    build apis for cupon ,  payment and payment  and stripe

--- src/app.ts
+++ src/app.ts
@@ -4,15 +4,15 @@ import { errorMiddleware } from "./middlewares/error.js";
 import NodeCache from "node-cache";
 import { config } from "dotenv";
 import morgan from "morgan";
-// import Stripe from "stripe";
+import Stripe from "stripe";
 // import cors from "cors";
 
 // Importing Routes
 import userRoute from "./routes/user.js";
 import productRoute from "./routes/products.js";
 import orderRoute from "./routes/order.js";
-// import paymentRoute from "./routes/payment.js";
-// import dashboardRoute from "./routes/stats.js";
+import paymentRoute from "./routes/payment.js";
+import dashboardRoute from "./routes/stats.js";
 
 config({
   path: "./.env",
@@ -20,11 +20,11 @@ config({
 // console.log(process.env.PORT);
 const port = process.env.PORT || 4000;
 const mongoURI = process.env.MONGO_URI || "";
-// const stripeKey = process.env.STRIPE_KEY || "";
+const stripeKey = process.env.STRIPE_KEY || "";
 
 connectDB(mongoURI);
 
-// export const stripe = new Stripe(stripeKey);
+export const stripe = new Stripe(stripeKey);
 export const myCache = new NodeCache();
 
 const app = express();
@@ -41,8 +41,8 @@ app.get("/", (req, res) => {
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/product", productRoute);
 app.use("/api/v1/order", orderRoute);
-// app.use("/api/v1/payment", paymentRoute);
-// app.use("/api/v1/dashboard", dashboardRoute);
+app.use("/api/v1/payment", paymentRoute);
+app.use("/api/v1/dashboard", dashboardRoute);
 
 app.use("/uploads", express.static("uploads"));
 app.use(errorMiddleware);
